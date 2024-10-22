e0d5f480fafb5ec630c4b09a91b3b9bd5f278e5f
KumawatRameshwar <53176504+RameshwarKumawat@users.noreply.github.com>
2024-10-21 22:12:21 -0400

    Create ico.py

---
+++ ico.py
@@ -0,0 +1,29 @@
+'''Python script to generate Iocsahedron shape silver nanoparticles'''
+from ase.cluster import Icosahedron
+from ase.io import write
+
+# Function to generate and write icosahedral clusters
+def generate_icosahedral_clusters(symbol, sizes):
+    for size in sizes:
+        cluster = Icosahedron(symbol, noshells=size)
+        filename = f'{symbol}{len(cluster)}.xyz'
+        write(filename, cluster)
+        print(f'{filename} created with {len(cluster)} atoms.')
+
+# Sizes corresponding to the number of shells required for each cluster
+sizes = {
+    13: 1,
+    55: 2,
+    147: 3,
+    309: 4,
+    561: 5,
+    561: 6,
+    923: 7,
+    1415: 8,
+    2057: 9,
+    2869: 10,
+}
+
+# Generate and write clusters
+generate_icosahedral_clusters('Ag', sizes.values())
+
